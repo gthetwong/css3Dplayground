6836ec96ff3070127827a967f0c355f96c14bd35
gthetwong <gthetwong@gmail.com>
2014-04-30 18:59:33 -0700

    initial commit, added site controller, and index view

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Css3d::Application.config.secret_key_base = 'f3ee42207551552649faba64880c2520d41607e4370b2d1c5e5da2d94ef0c0c73168482392400a8183148bec9691ebeebde5860a6bfa79f3485924596ed6f80e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Css3d::Application.config.session_store :cookie_store, key: '_css3d_session'
